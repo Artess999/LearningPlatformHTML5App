ad118328e0c5135cd41a5e98723474185a4def29
Artem Kononenko <artess999@gmail.com>
2017-05-13 17:03:38 +0300

    server interaction module

--- js/server_interaction.js
+++ js/server_interaction.js
@@ -5,19 +5,21 @@
 class Server {
     constructor() {
         this.bookId = 1;
+        this.addr = 'http://elbook.ml/api/rest/';
+
         this._initial();
     }
 
     _initial() {
         let required = ['title', 'copyright'];
-        $.ajax('http://elbook.ml/api/rest/getparametrs', {
+        $.ajax(this.addr + 'getparametrs', {
             data: {"bookId": this.bookId, "params": required.toString()},
             dataType: "json",
             success: function (res) {
                 console.log(res);
-                if (res.code === 200){
+                if (res.code === 200) {
                     res.response.forEach(function (param) {
-                        switch (param.name){
+                        switch (param.name) {
                             case 'title':
                                 $('#title').html(param.value);
                                 break;
@@ -26,10 +28,13 @@ class Server {
                                 break;
                         }
                     })
+                } else {
+                    alert("Произошла ошибка! Номер ошибки: " + res.code);
                 }
             },
             error: function (xmlhttprequest, textstatus, message) {
                 console.log(xmlhttprequest, textstatus, message);
+                alert("Произошла ошибка!");
             }
         })
     }
