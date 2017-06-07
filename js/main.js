65d2bc595a022b09eb90adb3c52a9358555c6ea1
Artess999 <artem199521@bk.ru>
2017-06-07 14:30:00 +0300

    Small improvements

--- js/main.js
+++ js/main.js
@@ -101,47 +101,10 @@ function indexOnClick() {
             }, contentAnimationTime)
         }
     } else {
-        $("aside").collapse("toggle");
-    }
-}
-
-/*
-$(".next").click(function () {
-    $.getJSON("http://localhost:63342/LearningPlatformHTML5App/JSONProvider.php?id=2",
-        function (data) {
-            $(".currentPageText").text(data.text)
-        })
-});
-
-$(".previous").click(function () {
-    $.getJSON("http://localhost:63342/LearningPlatformHTML5App/JSONProvider.php?id=1",
-        function (data) {
-            $(".currentPageText").text(data.text)
-        })
-});
-*/
-
-let elems = document.getElementById("index-list").childNodes;
-
-function BuildList(elems) {
-    for (let elem of elems) {
-        if (typeof elem.tagName === "string" && elem.id.startsWith("for")) {
-            for (let el of elem.childNodes) {
-                if (typeof el.tagName === "string" && el.id.startsWith("p")) {
-                    elem.onclick = function (e) {
-                        let classes = el.classList;
-                        classes.contains("collapse") ? classes.remove("collapse") : classes.add("collapse");
-                        e.stopPropagation();
-                    };
-                    BuildList(el.childNodes);
-                }
-            }
+        try {
+            $("aside").collapse("toggle");
+        }
+        catch (e) {
         }
     }
 }
-
-BuildList(elems);
-
-$("ul li").click(function (e) {
-    e.stopPropagation();
-});
